d201b7e500c6d78c6987ab1578e662b549b02617
Brad Barrows <bradleyb1537@gmail.com>
2018-12-02 17:27:53 -0800

    websockets serving 2 pages

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var port = process.env.PORT || 3000;
+
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+const port = process.env.PORT || 3000;
+
+let pageNumber = 1;
+
+app.use(express.static('public'))
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+  //console.log(req);
+  res.sendFile(__dirname + `/page${pageNumber}.html`);
 });
 
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
+    io.emit('pageChange', 'page2.html');
+    pageNumber = 2;
   });
 });
 
